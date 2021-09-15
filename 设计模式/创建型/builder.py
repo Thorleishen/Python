fcfff963ebcfa1b57ecd39fca85385fe72e355ff
leizhenyu <907602907@qq.com>
2021-09-15 09:40:19 +0800

    :sparkles: factory

---
+++ 设计模式/创建型/builder.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2021/9/14 9:59 上午
+# @Author : zhenyu lei
+# @File : builder.py
+# @desc :
+
+
+class Builder(object):
+    pass
+
+
+
--- 设计模式/创建型/单例模式/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# _*_ coding: utf-8 _*_
-# @Time : 2021/9/10 10:05 上午
-# @Author : zhenyu lei
-# @File : __init__.py.py
-# @desc :
